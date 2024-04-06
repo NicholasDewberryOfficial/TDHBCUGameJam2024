9d86ea01f0bbf2f194aef1978ca702b592e9637d
AlienwareDeww <coolguynickyd@gmail.com>
2024-04-06 14:47:55 -0400

    game upload

---
+++ GameFolder/RPGStuff/emyres.gd
@@ -0,0 +1,28 @@
+extends Resource
+
+class_name emyres
+
+#@export var name 
+@export var texture: Texture2D
+@export var damage: int
+@export var health: int 
+#defences [0] = bash attack
+#[1] = poke attack 
+#[2] and on are left open
+
+#so if defences[0] = 0 no change
+#defences[1] = weakness (double damage) 
+#defences[2] = resistance (half damage)
+@export var defences: Array[int]
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	
+	
