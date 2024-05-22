b963d2305036006ae1067f54b0b785b1238d89d9
AlienwareDeww <coolguynickyd@gmail.com>
2024-05-22 12:10:35 -0400

    tower placement working

--- GameFolder/revampedTDSection/towerplacement.gd
+++ GameFolder/revampedTDSection/towerplacement.gd
@@ -6,15 +6,26 @@ var currtower = null
 var transphammergirl = load("res://revampedTDSection/towers/placetowergirl.tscn")
 var hammgirlprefab = load("res://revampedTDSection/towers/hammergirl.tscn")
 var placeme = null
-# Called when the node enters the scene tree for the first time.
+
+@export var totowerholder: Node
+
+@export var tmap: TileMap
+var tdata: TileSet
+@export var hgirlcost:int = 100 
+@export var pphpholder: RichTextLabel = null
+
+
 func _ready():
+	#totowerholder = get_node()
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	pphpholder.text = "[b] PP: [/b]" + str(Globalvars.pp) + "\n [b] HP: [/b]" + str(Globalvars.corehp)
 	if(placeme !=null):
 		placeme.global_position = get_viewport().get_mouse_position()
+		tdata = tmap.tile_set
 		#print("should be changing in towerplacement script")
 		
 	if(currtower != null and placeme ==null):
@@ -25,11 +36,17 @@ func _process(delta):
 	pass
 
 func _input(event):
-	if (event.is_action("lclick") and placeme!=null):
+	if (event.is_action("lclick") and placeme!=null and placeme.placable ==true):
 		match(currtower):
 			1:
+				if(Globalvars.pp < 100):
+					#logic saying youre poor and cant afford this 
+					return 
+				Globalvars.pp -=100 
 				var thisham = hammgirlprefab.instantiate()
-				add_child(thisham)
+				thisham.position = get_viewport().get_mouse_position()
+				
+				totowerholder.add_child(thisham)
 				currtower=null
 				placeme.queue_free()
 				placeme=null
@@ -39,3 +56,10 @@ func _input(event):
 func _on_hammergirlpanel_pressed():
 	currtower = 1
 	pass # Replace with function body.
+
+func checkifpositionworks() -> bool:
+	var pos = get_viewport().get_mouse_position()
+	#Here we would see if the position is valid or not. 
+	return true #or return false
+	
+	
--- GameFolder/revampedTDSection/towers/placetowergirl.gd
+++ GameFolder/revampedTDSection/towers/placetowergirl.gd
@@ -4,7 +4,8 @@ extends Node2D
 #@export var ttype : int = 1 
 
 
-
+var currTilemap
+var placable = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -15,3 +16,36 @@ func _process(delta):
 	global_position = get_viewport().get_mouse_position()
 	#print("Should be changing in placetowergirlscript")
 	pass
+
+
+func _on_area_2d_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
+	#if(body is TileMap):
+		#currTilemap = TileMap
+		#var tilecoords = currTilemap.get_coords_for_body_rid(body_rid)
+		#var mydata = currTilemap.get_custom_data_by_layer_id()
+	pass # Replace with function body.
+#https://piped.video/watch?v=k9RsnbP4a0c&ab_channel=DevDuck
+
+
+#func _on_area_2d_area_entered(area):
+	#placable = false 
+	#print("COLLIDED AREA ENTER ")
+	#pass # Replace with function body.
+#
+#
+#func _on_area_2d_area_exited(area):
+	#placable = true
+	#print("COLLIDED AREA EXIT")
+	#pass # Replace with function body.
+
+
+func _on_area_2d_body_entered(body):
+	placable = false 
+	print("COLLIDED BODY ENTER ")
+	pass # Replace with function body.
+
+
+func _on_area_2d_body_exited(body):
+	placable = true
+	print("COLLIDED BODY EXIT")
+	pass # Replace with function body.
