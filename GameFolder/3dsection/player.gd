24605b5868ff042649623e9713164129e43bb137
AlienwareDeww <coolguynickyd@gmail.com>
2024-06-06 13:33:07 -0400

    Feat: UpdatedFPSController
    HE STILL FALLS THRU FLOOR

--- GameFolder/3dsection/player.gd
+++ GameFolder/3dsection/player.gd
@@ -7,17 +7,30 @@ func _input(event):
 		rotation_degrees.y -= event.relative.x * MOUSE_SENS
 
 func _process(delta):
-	if Input.is_action_just_pressed("exit"):
-		get_tree().quit()
+	#if Input.is_action_just_pressed("exit"):
+	#	get_tree().quit()
+	pass
 
 func _physics_process(delta):
-	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+	var input_dir = Input.get_vector("left", "right", "forward", "backward")
+	velocity.y -= 9.8 * delta 
+	get_input(delta)
+	
+	#var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	#if direction:
+		#velocity.x = direction.x * SPEED
+		#velocity.z = direction.z * SPEED
+	#else:
+		#velocity.x = move_toward(velocity.x, 0, SPEED)
+		#velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+
+func get_input(delta):
+	var vy = velocity.y
+	velocity = Vector3.ZERO
+	var move = Input.get_axis("back", "forward")
+	var turn = Input.get_axis("right", "left")
+	velocity += -transform.basis.z * move * SPEED
+	rotate_y(MOUSE_SENS * turn * delta)
+	velocity.y = vy
