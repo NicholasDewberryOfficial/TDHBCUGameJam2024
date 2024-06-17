66478b8823bbbe46c9ad85399515b3fee0e4a092
AlienwareDeww <coolguynickyd@gmail.com>
2024-06-17 16:44:53 -0400

    updated sky

--- GameFolder/3dsection/basicbasicplayer.gd
+++ GameFolder/3dsection/basicbasicplayer.gd
@@ -11,13 +11,14 @@ func _physics_process(delta):
 	var movement_dir = transform.basis * Vector3(input.x, 0, input.y)
 	velocity.x = movement_dir.x * speed
 	velocity.z = movement_dir.z * speed
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 	move_and_slide()
 	if is_on_floor() and Input.is_action_just_pressed("jump"):
 		velocity.y = jump_speed
 		
 func _input(event):
-	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+	if event is InputEventMouseMotion:# and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
 		rotate_y(-event.relative.x * mouse_sensitivity)
 		$Camera3D.rotate_x(-event.relative.y * mouse_sensitivity)
 		$Camera3D.rotation.x = clampf($Camera3D.rotation.x, -deg_to_rad(70), deg_to_rad(70))
--- GameFolder/revampedTDSection/towers/hammergirl/hammerdmghitbox.gd
+++ GameFolder/revampedTDSection/towers/hammergirl/hammerdmghitbox.gd
@@ -4,7 +4,9 @@ extends Area2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$CPUParticles2D.restart()
+	#$CPUParticles2D.restart()
+	#print(str($CPUParticles2D.emitting))
+	$CPUParticles2D.emitting = true
 	pass
 
 
