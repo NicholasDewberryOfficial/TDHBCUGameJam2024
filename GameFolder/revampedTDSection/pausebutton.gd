bd0de9e0737ae9932415ea81e00af1623b116929
TyRtic1ce <150709020+TyRtic1ce@users.noreply.github.com>
2024-08-02 22:45:01 -0400

    Level Select Stuff

--- GameFolder/revampedTDSection/pausebutton.gd
+++ GameFolder/revampedTDSection/pausebutton.gd
@@ -1,15 +1,7 @@
 extends Button
 
 
-var paused: bool = false
-@export var children: Panel = null
-func _on_pressed():
-	get_tree().paused = !(get_tree().paused)
-	self.paused = false
-	pass # Replace with function body.
 
-func _process(delta):
-	if(get_tree().paused == true):
-		children.visible = true
-	else:
-		children.visible = false
+
+func _on_pressed():
+	pass # Replace with function body.
